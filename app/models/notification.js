44311b5c736108014fa469ab7eccd9647b5cae09
Cody Swann <cody@gunnertech.com>
2016-01-06 21:06:11 -0800

    Minor

--- app/models/notification.js
+++ app/models/notification.js
@@ -15,8 +15,8 @@ schema.post('save', function(doc) {
   require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD).send({
     to:       ['cody@gunnertech.com','hozencool@gmail.com'],
     from:     'no-reply@dropmic.com',
-    subject:  notificationSubject,
-    text:     notificationMessage
+    subject:  doc.notificationSubject,
+    text:     doc.notificationMessage
   }, function(err, json) {
     if (err) { return console.error(err); }
     console.log(json);
