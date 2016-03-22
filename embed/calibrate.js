908d6c86240c382cd144d5a0698f09fd3e1ee2a5
Mahdi Hussein <m@hdihusse.in>
2016-03-22 19:14:47 -0400

    Use sox for rms calculations

--- embed/calibrate.js
+++ embed/calibrate.js
@@ -1,13 +1,21 @@
-// Create a new instance of node-core-audio 
-var coreAudio = require("node-core-audio");
 var fs = require('fs');
 var childProcess = require('child_process');
 var path = require('path');
  
-// Create a new audio engine 
-var engine = coreAudio.createNewAudioEngine();
 var samples = [];
 var savedTrimValue = null;
+
+var readline = require('readline');
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', function(line){
+    var rms = parseFloat(line.trim());
+    processAudio(rms)
+})
 
 function runScript(scriptPath, callback) {
 
@@ -60,30 +68,9 @@ function getVariance( numArr, numOfDec ){
 	return getNumWithSetDec( v, numOfDec );
 }
  
-// Add an audio processing callback 
-// This function accepts an input buffer coming from the sound card, 
-// and returns an ourput buffer to be sent to your speakers. 
-// 
-// Note: This function must return an output buffer 
-function processAudio( inputBuffer ) {
-  if(savedTrimValue) { return inputBuffer; }
+function processAudio(rms) {
+  if(savedTrimValue) { return; }
   
-  var input = inputBuffer[0]
-  , len = input.length
-  , total = i = 0
-  , rms
-  , trim
-  , dBFS
-  
-  
-  console.log("~~~~~~~~~LENGTH: " + len)
-  
-  for ( var j = 0; j < len; j = j + 2) {
-    var sample = input[j] // 32768.0
-    total += (sample * sample);
-  }
-  
-  rms = Math.sqrt(total / (len / 2));
   expectedDBFS = 94.0;
   actualDBFS = 20 * Math.log10(rms);
   trim = expectedDBFS + actualDBFS
@@ -116,26 +103,7 @@ function processAudio( inputBuffer ) {
           if (err) throw err;
           console.log('finished run.js');
         });
-        
     }); 
   }
-  
-  
-  
-
-  return inputBuffer;
 }
 
-for(var i=0; i<engine.getNumDevices(); i++) {
-  console.log(engine.getDeviceName(i));
-}
-
-
-engine.setOptions({
-  inputChannels: 1,
-  inputDevice: 5, ///THIS VALUE WILL HAVE TO BE CHANGED TO MATCH THE APPROPRIATE INPUT DEVICE
-  outputChannels: 1
-});
-
-
-engine.addAudioCallback( processAudio );
